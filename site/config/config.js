196c6458dd29cef9d6c7d2cc26bdcb3f3204e058
dharaluxe <krishhtri7@gmail.com>
2025-08-19 14:49:32 +0530

    Create config.js

---
+++ site/config/config.js
@@ -0,0 +1,6 @@
+// site/config/config.js
+window.PLAYNITI_SUPABASE = {
+  url: "https://rxqrreyhoyjbigpfpep.supabase.co",
+  anon: "sb_publishable_W-kSQcUCpcW_z5BgVohjgw_39uqhluI",
+  adminEmail: "Krishhtri@gmail.com"
+};
