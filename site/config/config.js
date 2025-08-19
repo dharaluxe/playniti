f81b6a37578317602391bd1b09fa9bfce10e842a
dharaluxe <krishhtri7@gmail.com>
2025-08-19 14:57:10 +0530

    config.js

--- site/config/config.js
+++ site/config/config.js
@@ -1,6 +1,6 @@
 // site/config/config.js
 window.PLAYNITI_SUPABASE = {
-  url: "https://rxqrreyhoyjbigpfpep.supabase.co",
-  anon: "sb_publishable_W-kSQcUCpcW_z5BgVohjgw_39uqhluI",
-  adminEmail: "Krishhtri@gmail.com"
+  url: "https://rxqrreyhoyjbigpfpep.supabase.co",   // Supabase Project URL
+  anonKey: "sb_publishable_W-kSQcUCpcW_z5BgVohjgw_39uqhluI", // Supabase anon/public key
+  adminEmail: "Krishhtrig@gmail.com"  // Admin login email
 };
