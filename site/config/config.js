4b00984fc7985485593857d5a471e2336044a22b
dharaluxe <krishhtri7@gmail.com>
2025-08-28 15:05:53 +0530

    Delete site directory

--- site/config/config.js
+++
@@ -1,6 +0,0 @@
-// site/config/config.js
-window.PLAYNITI_SUPABASE = {
-  url: "https://rxqrreyhoyjbigpfpep.supabase.co",   // Supabase Project URL
-  anonKey: "sb_publishable_W-kSQcUCpcW_z5BgVohjgw_39uqhluI", // Supabase anon/public key
-  adminEmail: "Krishhtrig@gmail.com"  // Admin login email
-};
--- site/service-worker.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener('install', (e)=>{
-  e.waitUntil(caches.open('playniti-v1').then(c => c.addAll([
-    '/site/index.html','/site/dashboard.html','/site/game.html',
-    '/site/css/styles.css','/site/manifest.json'
-  ])));
-});
-self.addEventListener('fetch', (e)=>{
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
-});
